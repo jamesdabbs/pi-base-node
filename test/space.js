5a9d4fa733b9ee32ad30722c3023d97816db7eca
James Dabbs <jamesdabbs@gmail.com>
2014-11-05 12:54:32 -0500

    Extract Resource module

--- test/space.js
+++ test/space.js
@@ -3,7 +3,25 @@
 var Space = require('../lib/space');
 
 describe('Space', function () {
-  it('can list spaces', function() {
+  it('can list', function() {
     expect(Space.list()).to.eventually.have.length(50);
   });
+
+  it('can get a page at a time');
+  it('can choose the page size');
+  it('caps the page size');
+
+  it('can find by id', function() {
+    expect(Space.find(1)).to.eventually.have.property('name', 'Space 1');
+  });
+
+  it('requires authentication to create');
+  it('allows logged-in users to create');
+  it('validates create data');
+  it('requires admin access to update');
+  it('allows admins to update');
+  it('validates updates');
+  it('tracks revisions');
+  it('requires admin access to delete');
+  it('allows admins to delete');
 });
