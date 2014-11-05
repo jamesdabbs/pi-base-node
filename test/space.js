aa2ad089f673edb61455af08aed5411025fa14e5
James Dabbs <jamesdabbs@gmail.com>
2014-11-05 18:37:47 -0500

    Flesh out pagination methods

--- test/space.js
+++ test/space.js
@@ -7,28 +7,46 @@ describe('Space', function () {
     expect(Space.list()).to.eventually.have.length(50);
   });
 
-  it('can get a page at a time');
-  it('can choose the page size');
-  it('caps the page size');
+  it('can get a page at a time', function() {
+    Space.list({page: 2}).then(function(spaces) {
+      expect(spaces[0].name).to.eq('Space 54');
+    });
+  });
+
+  it('can choose the page size', function() {
+    expect(Space.list({perPage: 10})).to.eventually.have.length(10);
+  });
+
+  it('caps the page size', function() {
+    expect(Space.list({perPage: 100})).to.eventually.have.length(50);
+  });
 
   it('can find by id', function() {
     expect(Space.find(1)).to.eventually.have.property('name', 'Space 1');
   });
 
-  it('requires authentication to create');
+  it('requires authentication to create', function() {
+    expect(Space.create({name: 'New Space'})).to.be.rejectedWith('401');
+  });
+
   it('allows logged-in users to create');
   it('validates create data');
 
-  it('requires admin access to update');
+  it.skip('requires admin access to update', function() {
+    Space.find(7).then(function(space) {
+      space.update({description: 'Updated description'}).then(function() {
+        expect(space.description).to.eq('Updated description');
+      });
+    });
+  });
 
   it('allows admins to update');
   it('validates updates');
   it('tracks revisions');
 
-  it('requires admin access to delete', function() {
+  it.skip('requires admin access to delete', function() {
     Space.find(99).then(function(space) {
-      // FIXME: ensure that we reject with somethign reflecting the forbidden status
-      expect(space.delete()).to.be.rejected;
+      expect(space.delete()).to.be.rejectedWith('403');
     });
   });
 
--- test/support/common.js
+++ test/support/common.js
@@ -6,4 +6,3 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 global.expect = chai.expect;
-global._ = require('underscore');
